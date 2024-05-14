07f5b2dd4e37905567fdd8be2680609eb7781255
NamanAnand13 <namananand2003@gmail.com>
2024-05-14 17:00:23 +0530

    home page created

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import EditorPage from './pages/EditorPage'
+import { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <>
+      <Toaster position='top-right'
+        toastOptions={
+          {
+            success: {
+              iconTheme: {
+                primary: '#258BFA',
+              }
+            }
+          }
+        }
+      ></Toaster>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidV4 } from 'uuid';
+import toast, { Toaster } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const [roomId, setRoomId] = useState('');
+    const [userName, setUserName] = useState('');
+    const navigate = useNavigate();
+    // const [clicked, setClicked] = useState(false);
+
+    const handleClick = (e) => {
+        console.log(e.code);
+        // console.log(e);
+        if (e.code === "Enter") {
+            joinRoom();
+        }
+    };
+
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidV4();
+        // console.log(id);
+        setRoomId(id);
+        toast.success('Created new room');
+    };
+
+    const joinRoom = () => {
+        if (!roomId || !userName) {
+            toast.error("Room ID and user name required");
+            return;
+        }
+        navigate(`/editor/${roomId}`, { state: { userName } });
+    }
+
     return (
         <div class="HomePageWrapper">
             <div class="FormWrapper">
                 <img className='homePageLogo' src="/CoDeC.png" alt="CoDeC-Logo"></img>
                 <h4 className='mainLabel'>Paste invitation Room ID</h4>
                 <div className="inputGroup">
-                    <input type="text" className='inputBox' placeholder='Room ID'></input>
-                    <input type="text" className='inputBox' placeholder='USER NAME'></input>
-                    <button className='btn joinBtn'>Join</button>
+                    <input type="text" className='inputBox' placeholder='Room ID' value={roomId} onChange={
+                        (e) => setRoomId(e.target.value)
+                    }
+                        onKeyDown={handleClick}
+                    ></input>
+                    <input type="text" className='inputBox' placeholder='USER NAME' value={userName} onChange={
+                        (e) => setUserName(e.target.value)
+                    }
+                        onKeyDown={handleClick}
+                    ></input>
+                    <button className='btn joinBtn' onClick={joinRoom}>Join</button>
                     <span className='createInfo'>
                         If you don't have an invite then create &nbsp;
-                        <a href="" className='createNewBtn'>new room</a>
+                        <a href="" onClick={createNewRoom} className='createNewBtn'>new room</a>
                     </span>
                 </div>
             </div>
