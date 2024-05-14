669c2da877b4b96d8a27b811501d186dd69f258f
NamanAnand13 <namananand2003@gmail.com>
2024-05-15 02:45:53 +0530

    editor page layout created

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Client from '../components/Client';
+import Editor from '../components/editor';
 
 const EditorPage = () => {
+    const [clients, setClients] = useState([
+        { socketId: 1, username: "naman a" },
+        // { socketId: 2, username: "hims jain" },
+        // { socketId: 3, username: "faiz" },
+    ]);
     return (
-        <div>EditorPage</div>
+        <div className='mainWrap'>
+            <div className='aside'>
+                <div className='asideInner'>
+                    <div className='logo'>
+                        <img src='/CoDeC.png' alt='codec-logo' className='logoEditor'></img>
+                    </div>
+                    <h3>Connected</h3>
+                    <div className='userList'>
+                        {
+                            clients.map((client) => (
+                                <Client username={client.username} key={client.socketId} />
+                            ))
+                        }
+                    </div>
+                </div>
+                <button className='btn copyBtn'>Copy ROOM ID</button>
+                <button className='btn LeaveBtn'>Leave</button>
+            </div>
+            <div className='editorWrap'>
+                <Editor></Editor>
+            </div>
+        </div>
     )
 }
 
